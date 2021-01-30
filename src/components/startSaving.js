acd9c5e29b915e476aec8a1092016e645dc66c01
toni-li <toni.li@uwaterloo.ca>
2021-01-29 21:04:56 -0500

    added bolding and text

--- src/components/startSaving.js
+++ src/components/startSaving.js
@@ -1,47 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const Form = () => {
-
-    var tabs = document.getElementsByClassName('Tab');
-
-    Array.prototype.forEach.call(tabs, function(tab) {
-        tab.addEventListener('click', setActiveClass);
-    });
-
-    function setActiveClass(evt) {
-        Array.prototype.forEach.call(tabs, function(tab) {
-            tab.classList.remove('active');
-        });
+class startSaving extends Component {
+    render() {
         
-        evt.currentTarget.classList.add('active');
+        var tabs = document.getElementsByClassName('Tab');
+        window.addEventListener('load',function(){
+            Array.prototype.forEach.call(tabs, function(tab) {
+                tab.addEventListener('click', setActiveClass);
+            });
+        });
+    
+        function setActiveClass(evt) {
+            Array.prototype.forEach.call(tabs, function(tab) {
+                tab.classList.remove('active');
+            });
+            
+            evt.currentTarget.classList.add('active');
+        }
+    
+        function showTab(selected, total)
+        {
+            var i;
+          for(i = 1; i <= total; i += 1)
+          {
+            document.getElementById('tabs-' + i).style.display = 'none';
+          }
+          document.getElementById('tabs-' + selected).style.display = 'block';
+        }
+
+        return (
+            <div class="Panel">
+                <nav>
+                    <ul class="Tabs">
+                        <li class="Tabs__tab active Tab" id="tabs1" onClick={() => { showTab(1,2);}}><a href="#">Solar</a></li>
+                        <li class="Tabs__tab Tab" id="tabs2" onClick={() => { showTab(2,2);}}><a href="#">Solar + Battery</a></li>
+                        <hr id="tab-underline"></hr>
+                        <li class="Tabs__presentation-slider" role="presentation"></li>
+                    </ul>
+                </nav>
+
+                
+
+                <p id="solar-subtext"> I don’t own a home battery </p>
+                <p id="battery-subtext"> I own a home battery </p>
+
+                <p id="form-desc"> The information you provide will help determine the best solar system specifications for your home.</p>
+                <p id="form-desc2"> In the case where you are not able to retrieve a field, we will use a default value for the analysis.</p>
+
+                <p class="Panel__body tabContent" id="tabs-1"> Solar Form </p>
+                <p class="Panel__body tabContent" id="tabs-2"> Solar + Battery Form </p>
+
+            </div>
+        )
     }
-
-    function showTab(selected, total)
-    {
-
-        var i;
-      for(i = 1; i <= total; i += 1)
-      {
-        document.getElementById('tabs-' + i).style.display = 'none';
-      }
-      document.getElementById('tabs-' + selected).style.display = 'block';
-    }
-
-    return (
-       <div class="Panel">
-            <nav>
-                <ul class="Tabs">
-                    <li class="Tabs__tab active Tab" id="tabs1" onClick={() => { showTab(1,2);}}><a href="#">Solar</a></li>
-                    <li class="Tabs__tab Tab" id="tabs2" onClick={() => { showTab(2,2);}}><a href="#">Solar + Battery</a></li>
-                    <li class="Tabs__presentation-slider" role="presentation"></li>
-                </ul>
-            </nav>
-
-            <p class="Panel__body tabContent" id="tabs-1"> Toni</p>
-		    <p class="Panel__body tabContent" id="tabs-2"> is the best!! </p>
-
-       </div>
-    );
 }
- 
-export default Form;
+
+export default startSaving
