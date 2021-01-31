d6f981bd62c4c089a1920d4c793621419384d2ee
Ghazal Malekanian <Ghazal@Ghazals-MacBook-Pro.local>
2021-01-30 20:19:39 -0500

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+
+    
+    render() {
+        return (
+            <div class="footer">
+                <p class="footer-left">
+                    <a>SoliⒸ2020</a> &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+                    <a>Management Engineering Capstone Design Project</a>
+                </p>
+            </div>
+
+        );
+
+    }
+}
+    
+export default Footer
