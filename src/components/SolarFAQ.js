5d5165caffb012687f92e896ab3d6e899df75d54
Ghazal Malekanian <Ghazal@Ghazals-MacBook-Pro.local>
2021-02-20 13:44:43 -0500

    react accordian test

--- src/components/SolarFAQ.js
+++ src/components/SolarFAQ.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
+import Accordian from '../components/Accordion'
 
 class SolarFAQ extends Component {
     render() {
-        var defaultForm = "solar";
-        localStorage.setItem("whichForm", defaultForm);
         return (
         <div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
             <p id="faq-title"> Frequently Asked Questions </p>
             <p id="faq-desc"> Learn more about how solar energy works, how Soli calculates your custom solar system, and next steps to implement your solar investment. </p>
+        
+
+            <Accordian title="what is abc?" content="letters" />
+            <Accordian title="what is cbd?" content="hello" />
         </div>
         )
     }
