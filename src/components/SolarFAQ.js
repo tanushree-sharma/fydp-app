d7ad61739b921eee360b9e9248b992e429a583b2
Ghazal Malekanian <Ghazal@Ghazals-MacBook-Pro.local>
2021-02-23 12:43:06 -0500

    title format

--- src/components/SolarFAQ.js
+++ src/components/SolarFAQ.js
@@ -6,7 +6,12 @@ class SolarFAQ extends Component {
         <div>
             <p id="faq-title"> Frequently Asked Questions </p>
             <p id="faq-desc"> Learn more about how solar energy works, how Soli calculates your custom solar system, and next steps to implement your solar investment. </p>
-    
+            
+            <p id="aboutSolar"> About Solar Energy </p>
+            <p id="myData"> My Data </p>
+            <p id="myResults"> My Results </p>
+            <p id="nextSteps"> Next Steps </p>
+
         </div>
         )
     }
